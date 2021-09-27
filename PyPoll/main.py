177a6d8dd9a923af4427899f23c87a419f8cd641
Bill Johnson <wpj174@gmail.com>
2021-09-27 08:03:34 -0400

    Update PyPoll main.py
    Converted variable names from camelCase to snake_case.
    
    Need to finish comments.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,12 @@
 """
-* In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
+* In this challenge, you are tasked with helping a small, rural town modernize
+  its vote counting process.
 
-* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv).
-The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create 
-a Python script that analyzes the votes and calculates each of the following:
+* You will be give a set of poll data called [election_data.csv]
+  (PyPoll/Resources/election_data.csv).
+  The dataset is composed of three columns: `Voter ID`, `County`, and
+  `Candidate`. Your task is to create a Python script that analyzes the votes
+  and calculates each of the following:
 
   * The total number of votes cast
 
@@ -31,73 +34,65 @@ a Python script that analyzes the votes and calculates each of the following:
   -------------------------
   ```
 
-* In addition, your final script should both print the analysis to the terminal and export a text file
-with the results.
+* In addition, your final script should both print the analysis to the
+  terminal and export a text file with the results.
 """
 
+# Import required modules
 import os
 import csv
 
-inputPath = os.path.join("Resources", "election_data.csv")
-outputPath = os.path.join("analysis", "Election_Results.txt")
+# Define input and output file paths
+input_path = os.path.join("Resources", "election_data.csv")
+output_path = os.path.join("analysis", "Election_Results.txt")
 
-voteCount = 0
+vote_count = 0
 votes = dict()
 
 
-with open(inputPath) as csvFile:
-  csvReader = csv.reader(csvFile, delimiter=',')
-  csvHeader = next(csvReader)
-  for row in csvReader:
-    voteCount += 1
-    voteGetter = row[2]
-    if voteGetter in votes:
-        votes[voteGetter] += 1
+with open(input_path) as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  csv_header = next(csv_reader)
+  for row in csv_reader:
+    vote_count += 1
+    vote_getter = row[2]
+    if vote_getter in votes:
+        votes[vote_getter] += 1
     else:
-        votes[voteGetter] = 1
+        votes[vote_getter] = 1
 
-maxVotes = 0
-winner = ""
+max_votes = 0
 
-with open(outputPath,'w') as txtFile:
-    txtFile.write("Election Results\n")
+with open(output_path,'w') as txt_file:
+
+    txt_file.write("Election Results\n")
     print("Election Results")
-    txtFile.write("----------------------------\n")
-    print("----------------------------")
-    txtFile.write(f"Total Votes: {voteCount:,}\n")
-    print(f"Total Votes: {voteCount:,}")
-    txtFile.write("----------------------------\n")
-    print("----------------------------")
-    for voteGetter in votes:
-        votePct = (votes[voteGetter] / voteCount) * 100
-        txtFile.write(f"{voteGetter}: {votePct:.3f}% ({votes[voteGetter]:,})\n")
-        print(f"{voteGetter}: {votePct:.3f}% ({votes[voteGetter]:,})")
-        if votes[voteGetter] > maxVotes:
-            maxVotes = votes[voteGetter]
-            winner = voteGetter
-    txtFile.write("----------------------------\n")
-    print("----------------------------")
-    txtFile.write(f"Winner: {winner}\n")
-    print(f"Winner: {winner}")
-    txtFile.write("----------------------------\n")
-    print("----------------------------")
-    txtFile.write("---\n")
-    print("---")
-        
 
-"""
-  txtFile.write(f"Total Months: {monthCount}\n")
-  print(f"Total Months: {monthCount}")
-  txtFile.write(f"Total Net Profit: ${totalNetProfit:,.2f}\n")
-  print(f"Total Net Profit: ${totalNetProfit:,.2f}")
-  txtFile.write(f"Total Period Change in Profit: ${totalChange:,.2f}\n")
-  print(f"Total Period Change in Profit: ${totalChange:,.2f}")
-  txtFile.write(f"Average Change in Profit: ${totalChange/(monthCount-1):,.2f}\n")
-  print(f"Average Change in Profit: ${totalChange/(monthCount-1):,.2f}")
-  txtFile.write(f"Greatest Increase in Profits: {maxIncMonth} (${maxIncrease:,.2f})\n")
-  print(f"Greatest Increase in Profits: {maxIncMonth} (${maxIncrease:,.2f})")
-  txtFile.write(f"Greatest Decrease in Profits: {maxDecMonth} (${maxDecrease:,.2f})\n")
-  print(f"Greatest Decrease in Profits: {maxDecMonth} (${maxDecrease:,.2f})")
-  txtFile.write("---\n")
-  print("---")
-"""
+    txt_file.write("----------------------------\n")
+    print("----------------------------")
+
+    txt_file.write(f"Total Votes: {vote_count:,}\n")
+    print(f"Total Votes: {vote_count:,}")
+
+    txt_file.write("----------------------------\n")
+    print("----------------------------")
+
+    for vote_getter in votes:
+        votePct = (votes[vote_getter] / vote_count) * 100
+        txt_file.write(f"{vote_getter}: {votePct:.3f}% ({votes[vote_getter]:,})\n")
+        print(f"{vote_getter}: {votePct:.3f}% ({votes[vote_getter]:,})")
+        if votes[vote_getter] > max_votes:
+            max_votes = votes[vote_getter]
+            winner = vote_getter
+
+    txt_file.write("----------------------------\n")
+    print("----------------------------")
+
+    txt_file.write(f"Winner: {winner}\n")
+    print(f"Winner: {winner}")
+
+    txt_file.write("----------------------------\n")
+    print("----------------------------")
+
+    txt_file.write("---\n")
+    print("---")
